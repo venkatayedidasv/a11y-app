96a334fef4fabfccae9909676f66bb6b5889ab98
Venkata Yedida <venkata.yedida@ingka.ikea.com>
2023-10-10 22:06:17 +0200

    updated artifacts

--- src/ArtifactViewer.js
+++ src/ArtifactViewer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import JSZip from 'jszip';
 
 function ArtifactViewer() {
   const [jsonData, setJsonData] = useState(null);
@@ -29,10 +30,10 @@ function ArtifactViewer() {
         responseType: 'blob',
       });
 
-      if (response.status === 302) {
+      if (response.status >= 200 && response.status < 300) {
         // Read the log.json file from the ZIP
         const zipBlob = response.data;
-        const zip = new window.JSZip();
+        const zip = new JSZip(); // Use the imported JSZip constructor
         await zip.loadAsync(zipBlob);
 
         // Get the content of log.json
@@ -49,6 +50,36 @@ function ArtifactViewer() {
     }
   };
 
+  const renderTable = () => {
+    if (!jsonData || !jsonData.length) {
+      return <p>No data available.</p>;
+    }
+
+    // Assuming the JSON data is an array of objects with the same structure
+    const headers = Object.keys(jsonData[0]);
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {jsonData.map((item, index) => (
+            <tr key={index}>
+              {headers.map((header) => (
+                <td key={header}>{item[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
   return (
     <div>
       <h2>Artifact Viewer</h2>
@@ -56,7 +87,7 @@ function ArtifactViewer() {
       {showData && jsonData ? (
         <div>
           <h3>log.json Contents</h3>
-          <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+          {renderTable()}
         </div>
       ) : (
         <p>Click the button to view results.</p>
